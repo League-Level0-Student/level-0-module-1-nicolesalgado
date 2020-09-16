4a50a80b334970236f645b275327b6d23fd0436a
root <root@b1af13a3252d>
2020-09-16 01:26:14 +0000

    finish module 1 banana rock paper scissors

---
+++ src/_11_my_ages/MyAges.java
@@ -0,0 +1,11 @@
+package _11_my_ages;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class MyAges {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 12; i++) {
+			System.out.println(i );
+		}
+	}
+}
---
+++ src/_13_bananas_4eva/Bananas123.java
@@ -0,0 +1,10 @@
+package _13_bananas_4eva;
+
+public class Bananas123 {
+	public static void main(String[] args) {
+for (int i = 0; i < 1000 ; i++) {
+	
+	System.out.println("bananas123");
+}
+	}
+}
